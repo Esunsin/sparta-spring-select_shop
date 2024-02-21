66a6aa6ce710ca7e1acf5327d836ca29a9dbec9d
Esunsin <jinchan1148@gmail.com>
2024-02-21 14:20:46 +0900

    feat : 관심상품 최저가 등록

--- src/main/java/com/sparta/myselectshop/controller/ProductController.java
+++ src/main/java/com/sparta/myselectshop/controller/ProductController.java
@@ -1,14 +1,12 @@
 package com.sparta.myselectshop.controller;
 
+import com.sparta.myselectshop.dto.ProductMypriceRequestDto;
 import com.sparta.myselectshop.dto.ProductRequestDto;
 import com.sparta.myselectshop.dto.ProductResponseDto;
 import com.sparta.myselectshop.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
@@ -17,8 +15,14 @@ public class ProductController {
 
     private final ProductService productService;
 
+    //관심상품 등록
     @PostMapping("/products")
     public ProductResponseDto creatProduct(@RequestBody ProductRequestDto productRequestDto) {
         return productService.createProduct(productRequestDto);
+    }
+    //관심상품 희망 최저가 등록하기
+    @PostMapping("/products/{id}")
+    public ProductResponseDto updateProduct(@PathVariable(name = "id") Long id, @RequestBody ProductMypriceRequestDto productMypriceRequestDto){
+        return productService.updateProduct(id, productMypriceRequestDto);
     }
 }
--- src/main/java/com/sparta/myselectshop/service/ProductService.java
+++ src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -1,19 +1,35 @@
 package com.sparta.myselectshop.service;
 
+import com.sparta.myselectshop.dto.ProductMypriceRequestDto;
 import com.sparta.myselectshop.dto.ProductRequestDto;
 import com.sparta.myselectshop.dto.ProductResponseDto;
 import com.sparta.myselectshop.entity.Product;
 import com.sparta.myselectshop.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class ProductService {
+    public static final int MIN_MY_PRICE = 100;
+
     private final ProductRepository productRepository;
 
     public ProductResponseDto createProduct(ProductRequestDto productRequestDto){
         Product saved = productRepository.save(new Product(productRequestDto));
         return new ProductResponseDto(saved);
+    }
+
+
+    public ProductResponseDto updateProduct(Long productId, ProductMypriceRequestDto productMypriceRequestDto) {
+        int myprice = productMypriceRequestDto.getMyprice();
+        if(myprice < MIN_MY_PRICE){
+            throw new IllegalArgumentException("유효하지 않은 관심가격입니다. 최소 " + MIN_MY_PRICE + " 원 이상으로 설정해 주세요");
+        }
+        Product product = productRepository.findById(productId).orElseThrow(() -> new NullPointerException("해당상품을 찾을 수 없습니다"));
+        product.update(productMypriceRequestDto);
+        return new ProductResponseDto(product);
     }
 }
